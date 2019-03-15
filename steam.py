b44e52a8c8c58d204e5f240f7c0b145f8c10c82b
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-03-15 14:06:24 +0900

    Adding Description

--- steam.py
+++ steam.py
@@ -1,5 +1,6 @@
 import asyncio
 import discord
+import requests
 
 app = discord.Client()
 
@@ -13,15 +14,27 @@ async def on_ready():
     print(app.user.id)
     print("=============")
 
-    await app.change_presence(game=discord.Game(name="Steam", type=1))
+    await app.change_presence(game=discord.Game(name="st!help to get help", type=1))
 
 
 @app.event
 async def on_message(message):
     if message.author.bot:
         return None
-
-    if message.content == "st!help":
-        await app.send_message(message.channel, "Here")
+    msg = message.content.split(' ')
+    print(msg)
+    if msg[0] == "st!help":
+        em = discord.Embed(title='SteamBot', description='Thank you for using SteamBot')
+        em.add_field(name='st!help', value='Help! Call help when you don\'t know what to do.', inline=False)
+        em.add_field(name='st!game', value='Steam Game Functions', inline=False)
+        em.add_field(name='st!game recent (Player)', value='Get player\'s recent played game.', inline=False)
+        await app.send_message(message.channel, embed=em)
+        if len(msg) > 1:
+            await app.send_message(message.channel, "help 명령어는 st!help만 쓰셔도 작동합니다.")
+    elif msg[0] == "st!game":
+        if len(msg) == 1:
+            await app.send_message(message.channel, "Use more specified message.")
+        if msg[1] == 'recent':
+            await app.send_message(message.channel, "st!game recent.")
 
 app.run(token)
