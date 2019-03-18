ea7f50a8d9884b9667a3fc71762a4cc15c154a97
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-03-18 09:56:12 +0900

    Add invaild id detection

--- steam.py
+++ steam.py
@@ -22,7 +22,6 @@ async def on_message(message):
     if message.author.bot:
         return None
     msg = message.content.split(' ')
-    print(msg)
     if msg[0] == "st!help":
         em = discord.Embed(title='SteamBot', description='Thank you for using SteamBot')
         em.add_field(name='st!help', value='Help! Call help when you don\'t know what to do.', inline=False)
@@ -37,9 +36,14 @@ async def on_message(message):
         elif msg[1] == 'recent':
             if len(msg) == 3:
                 steamid = get_steam_id(msg[2])
+                if steamid == 0:
+                    await app.send_message(message.channel, "Use actual steam id.")
+                    return
                 recents = requests.get('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=B6137C92F67299965B5E6BF287ECA4AE&steamid=' + steamid + '&format=json')
                 recents = recents.json()
-                print(recents)
+                if recents['response']['total_count'] == 0:
+                    await app.send_message(message.channel, 'You didn\'t played any game in last 2 weeks or your steam profile is private.')
+                    return
                 em = discord.Embed(title='Here\'s your recent played game list, ' + message.author.name + '!', description='And again, Thanks for Using!\nYou played ' + str(recents['response']['total_count']) + ' games in last 2 weeks.')
                 for text in recents['response']['games']:
                     em.add_field(name=text['name'] + ' (' + str(text['appid']) + ')', value='Played for ' + str("%.2f" % (text['playtime_2weeks'] / 60)) + ' hours in last 2 weeks\nPlayed for ' + str("%.2f" % (text['playtime_forever'] / 60)) + ' hours in your lifetime', inline=False)
@@ -54,15 +58,17 @@ def get_steam_id(name):
         return name
     jsons = requests.get('https://api.steamid.uk/request.php?api=9295K4J61YKD3357E6WA&player=' + name +'&request_type=5&format=json')
     jsons = jsons.json()
+    if hasattr(jsons, 'error'):
+        return 0
 
     return jsons['linked_users']['steamid64']
 
 
 def isNumber(s):
-  try:
-    float(s)
-    return True
-  except ValueError:
-    return False
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 app.run(token)
