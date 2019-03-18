12c5d32193e4ef2f1def673f0acc7ecbfa5fbd9d
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-03-18 18:12:41 +0900

    add total played time
    - 총 플레이 시간 추가
    - 한글화

--- steam.py
+++ steam.py
@@ -23,34 +23,35 @@ async def on_message(message):
         return None
     msg = message.content.split(' ')
     if msg[0] == "st!help":
-        em = discord.Embed(title='SteamBot', description='Thank you for using SteamBot')
-        em.add_field(name='st!help', value='Help! Call help when you don\'t know what to do.', inline=False)
-        em.add_field(name='st!game', value='Steam Game Functions', inline=False)
-        em.add_field(name='st!game recent (Player)', value='Get player\'s recent played game.', inline=False)
+        em = discord.Embed(title='SteamBot', description='스팀봇을 사용해주셔서 감사합니다!')
+        em.add_field(name='st!help', value='도움! 무슨 명령어를 써야할지 모를 때 도움!을 외쳐주세요!', inline=False)
+        em.add_field(name='st!game recent (Player)', value='유저가 최근 2주간 플레이한 게임을 가져옵니다.', inline=False)
         await app.send_message(message.channel, embed=em)
         if len(msg) > 1:
             await app.send_message(message.channel, "help 명령어는 st!help만 쓰셔도 작동합니다.")
     elif msg[0] == "st!game":
         if len(msg) == 1:
-            await app.send_message(message.channel, "Use more specified message.")
+            await app.send_message(message.channel, "명령어를 제대로 입력해주세요!.")
         elif msg[1] == 'recent':
             if len(msg) == 3:
                 steamid = get_steam_id(msg[2])
                 if steamid == 0:
-                    await app.send_message(message.channel, "Use actual steam id.")
+                    await app.send_message(message.channel, "유효한 스팀 아이디를 사용해주세요.")
                     return
                 recents = requests.get('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=B6137C92F67299965B5E6BF287ECA4AE&steamid=' + steamid + '&format=json')
                 recents = recents.json()
                 if recents['response']['total_count'] == 0:
-                    await app.send_message(message.channel, 'You didn\'t played any game in last 2 weeks or your steam profile is private.')
+                    await app.send_message(message.channel, '어떠한 게임도 불러오지 못했어요. 아무런 게임도 플레이하지 않으셨을수도 있고, 스팀 프로필이 비공개일수도 있어요.')
                     return
-                em = discord.Embed(title='Here\'s your recent played game list, ' + message.author.name + '!', description='And again, Thanks for Using!\nYou played ' + str(recents['response']['total_count']) + ' games in last 2 weeks.')
+                em = discord.Embed(title='최근에 플레이하신 게임 목록입니다,, ' + message.author.name + ' 님!', description='그리고, 사용해주셔서 감사합니다!\n지난 2주간 ' + str(recents['response']['total_count']) + '개의 게임을 플레이하셨습니다..')
+                total_time = 0;
                 for text in recents['response']['games']:
-                    em.add_field(name=text['name'] + ' (' + str(text['appid']) + ')', value='Played for ' + str("%.2f" % (text['playtime_2weeks'] / 60)) + ' hours in last 2 weeks\nPlayed for ' + str("%.2f" % (text['playtime_forever'] / 60)) + ' hours in your lifetime', inline=False)
-
+                    total_time += text['playtime_2weeks'];
+                    em.add_field(name=text['name'] + ' (' + str(text['appid']) + ')', value='지난 2주간 ' + str("%.2f" % (text['playtime_2weeks'] / 60)) + ' 시간동안 플레이함\n평생 동안 ' + str("%.2f" % (text['playtime_forever'] / 60)) + ' 시간동안 플레이함', inline=False)
+                em.add_field(name='총 플레이 시간', value=msg[2] + '님은 지난 2주간 ' + str("%.2f" % (total_time / 60)) + '시간동안 플레이하셨습니다!')
                 await app.send_message(message.channel, embed=em)
             else:
-                await app.send_message(message.channel, "Enter Your ID")
+                await app.send_message(message.channel, "ID를 입력해주세요.")
 
 
 def get_steam_id(name):
