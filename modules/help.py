ca67e2acd8a3794f5f3996da98921681087adee9
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-05-04 23:42:18 +0900

    깔끔하게

--- modules/help.py
+++ modules/help.py
@@ -16,21 +16,21 @@ class Help:
             em = discord.Embed(title='SteamBot - st!game',
                                description='st!game에 대한 도움말이에요!\n[] => 비 필수 사항이고, 안에 있는 단어 중 하나를 선택해서 입력해주세요.\n() => 필수 사항이고, 위치에 맞는 형태로 입력해주세요.',
                                colour=discord.Colour(0x1b2838))
-            em.add_field(name='st!game new', value='스팀 최근 출시 제품들을 가져와요.', inline=False)
-            em.add_field(name='st!game specials', value='스팀 인기 할인 제품들을 가져와요.', inline=False)
-            em.add_field(name='st!game bestseller [ new, oncoming ]', value='스팀 인기 제품을 가져와요. `new`를 입력하면 신제품만 불러오고, `oncoming`을 입력하면 출시 예정 제품만 불러와요.', inline=False)
-            em.add_field(name='st!game hot', value='스팀 최다 플레이 중인 게임을 가져와요. 명령어 뒤에 숫자를 붙이면 입력한 만큼 가져와요.', inline=False)
-            em.add_field(name='st!game search ( query )', value='`query` 자리에 입력한 문장으로 스팀을 검색해요.', inline=False)
-            em.add_field(name='st!game realtime', value='해당 채널로 스팀 실시간 업데이트를 가져와요. 뒤에 `stop`을 붙이면 업데이트 수신을 중지해요.', inline=False)
+            em.add_field(name='new', value='스팀 최근 출시 제품들을 가져와요.', inline=False)
+            em.add_field(name='specials', value='스팀 인기 할인 제품들을 가져와요.', inline=False)
+            em.add_field(name='bestseller [ new, oncoming ]', value='스팀 인기 제품을 가져와요. `new`를 입력하면 신제품만 불러오고, `oncoming`을 입력하면 출시 예정 제품만 불러와요.', inline=False)
+            em.add_field(name='hot', value='스팀 최다 플레이 중인 게임을 가져와요. 명령어 뒤에 숫자를 붙이면 입력한 만큼 가져와요.', inline=False)
+            em.add_field(name='search ( query )', value='`query` 자리에 입력한 문장으로 스팀을 검색해요.', inline=False)
+            em.add_field(name='realtime', value='해당 채널로 스팀 실시간 업데이트를 가져와요. 뒤에 `stop`을 붙이면 업데이트 수신을 중지해요.', inline=False)
             return em
         elif select == 'user':
             em = discord.Embed(title='SteamBot - st!user',
                                description='st!user에 대한 도움말이에요!\n[] => 필수 사항이 아니고, 안에 있는 단어 중 하나를 선택해서 입력해주세요.\n() => 필수 사항이고, 위치에 맞는 형태로 입력해주세요.',
                                colour=discord.Colour(0x1b2838))
-            em.add_field(name='st!user profile ( username )', value='`username` 자리에 입력한 유저의 프로필을 가져와요.', inline=False)
-            em.add_field(name='st!user recent ( username )', value='`username` 자리에 입력한 유저가 최근 2주간 플레이한 게임을 가져와요.', inline=False)
-            em.add_field(name='st!user library ( username ) ', value='`username` 자리에 입력한 유저의 라이브러리를 10개 가져와요. 명령어 뒤에 숫자를 붙이면 입력한 만큼 가져와요. (최대 25개) 명령어 뒤에 `random`을 붙이면 랜덤한 한 게임을 가져와요.', inline=False)
-            em.add_field(name='st!user wishlist ( username )', value='`username` 자리에 입력한 유저의 찜 목록를 10개 가져와요. 명령어 뒤에 숫자를 붙이면 입력한 만큼 가져와요. (최대 50개)', inline=False)
+            em.add_field(name='profile ( username )', value='`username` 자리에 입력한 유저의 프로필을 가져와요.', inline=False)
+            em.add_field(name='recent ( username )', value='`username` 자리에 입력한 유저가 최근 2주간 플레이한 게임을 가져와요.', inline=False)
+            em.add_field(name='library ( username ) ', value='`username` 자리에 입력한 유저의 라이브러리를 10개 가져와요. 명령어 뒤에 숫자를 붙이면 입력한 만큼 가져와요. (최대 25개) 명령어 뒤에 `random`을 붙이면 랜덤한 한 게임을 가져와요.', inline=False)
+            em.add_field(name='wishlist ( username )', value='`username` 자리에 입력한 유저의 찜 목록를 10개 가져와요. 명령어 뒤에 숫자를 붙이면 입력한 만큼 가져와요. (최대 50개)', inline=False)
             return em
         elif select in command_list:
             em = discord.Embed(title='SteamBot - 상세 도움말이 없는 명령어', description='그 명령어는 상세 도움말이 없어요.', colour=discord.Colour(0x1b2838))
