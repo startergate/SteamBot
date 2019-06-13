56842a6853e25090cb2bb9c32e22d20d752cd4dc
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-06-13 21:17:56 +0900

    Seperate some functions

---
+++ modules/steamapi.py
@@ -0,0 +1,25 @@
+import requests
+import xml.etree.ElementTree as et
+from bs4 import BeautifulSoup
+
+def get_steam_id(name, want_all=False):
+    xmls = requests.get('https://steamcommunity.com/id/{}/?xml=1'.format(name)).text
+    xmls = et.fromstring(xmls)
+    try:
+        xmls.find('error').text
+        return 0
+    except AttributeError:
+        if want_all:
+            return xmls
+        return xmls.find('steamID64').text
+
+
+def get_game_id(name):
+    # Do Something
+    src = requests.get('https://store.steampowered.com/search/?term={}'.format(name.replace(' ', '+'))).text
+    src = BeautifulSoup(src, 'html.parser')
+    try:
+        target = src.find_all('a', class_='search_result_row')[0]
+    except IndexError:
+        return {}
+    return {target['href'].replace('https://store.steampowered.com/app/', '').split('/')[0]: target.find('span', class_='title').text}
--- steam.py
+++ steam.py
@@ -32,7 +32,6 @@ async def on_message(message):
         return None
     msg = message.content.split(' ')
     if msg[0] == "st!help":
-        help = Help()
         if len(msg) == 1:
             await app.send_message(message.channel, embed=help.getHelp())
         else:
@@ -377,29 +376,6 @@ async def on_message(message):
                                colour=statusColor).set_thumbnail(url=xmls.find('avatarIcon').text)
             em.add_field(name='요약', value=xmls.find('summary').text.replace('<br>', '\n'), inline=False)
             await app.send_message(message.channel, embed=em)
-
-
-def get_steam_id(name, want_all=False):
-    xmls = requests.get('https://steamcommunity.com/id/{}/?xml=1'.format(name)).text
-    xmls = et.fromstring(xmls)
-    try:
-        xmls.find('error').text
-        return 0
-    except AttributeError:
-        if want_all:
-            return xmls
-        return xmls.find('steamID64').text
-
-
-def get_game_id(name):
-    # Do Something
-    src = requests.get('https://store.steampowered.com/search/?term={}'.format(name.replace(' ', '+'))).text
-    src = BeautifulSoup(src, 'html.parser')
-    try:
-        target = src.find_all('a', class_='search_result_row')[0]
-    except IndexError:
-        return {}
-    return {target['href'].replace('https://store.steampowered.com/app/', '').split('/')[0]: target.find('span', class_='title').text}
 
 
 def isNumber(s):
