a2db7605ec17efae3556651bbbdd81759f8d5f22
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-10 19:52:28 +0900

    코드 스타일 개선 및 일부 로직 개선

--- steam.py
+++ steam.py
@@ -18,7 +18,6 @@ token = modules.setting.token
 
 loop = asyncio.get_event_loop()
 
-
 @app.event
 async def on_ready():
     print("로그인 정보>")
@@ -42,7 +41,8 @@ async def on_message(message):
         else:
             await app.send_message(message.channel, embed=help.getSpecificHelp(msg[1]))
     if msg[0] == 'st!add':
-        await app.send_message(message.channel, "아래 링크로 들어가서 SteamBot을 서버에 추가할 수 있어요!\n`https://discordapp.com/oauth2/authorize?client_id=555339236035919882&permissions=0&scope=bot`")
+        await app.send_message(message.channel,
+                               "아래 링크로 들어가서 SteamBot을 서버에 추가할 수 있어요!\n`https://discordapp.com/oauth2/authorize?client_id=555339236035919882&permissions=0&scope=bot`")
     elif msg[0] == "st!game":
         if len(msg) == 1:
             await app.send_message(message.channel, ":x: 명령어를 제대로 입력해주세요!.")
@@ -53,7 +53,8 @@ async def on_message(message):
                 return
 
             query = message.content.replace('st!game search ', '')
-            src = requests.get('https://store.steampowered.com/search/?term={}'.format(query.lower().replace(' ', '+'))).text
+            src = requests.get(
+                'https://store.steampowered.com/search/?term={}'.format(query.lower().replace(' ', '+'))).text
             src = BeautifulSoup(src, 'html.parser')
             games = src.find_all('a', class_='search_result_row')
             if not games:
@@ -64,11 +65,13 @@ async def on_message(message):
                 price = game.find('div', class_='search_price').getText().strip();
                 temp = price.split('₩')
                 if len(temp) >= 3:
-                    price = '₩ {} ~~₩ {}~~ ({})'.format(temp[2], temp[1], game.find('div', class_='search_discount').find('span').text)
+                    price = '₩ {} ~~₩ {}~~ ({})'.format(temp[2], temp[1],
+                                                        game.find('div', class_='search_discount').find('span').text)
                 if not price:
                     price = '기록 없음'
                 output_text += '\n' + game.find('span', class_='title').getText() + '  |  ' + price
-            em = discord.Embed(title='"{}"을 검색한 결과, {}개의 게임을 찾았어요!'.format(query, len(games)), description=output_text, colour=discord.Colour(0x1b2838))
+            em = discord.Embed(title='"{}"을 검색한 결과, {}개의 게임을 찾았어요!'.format(query, len(games)), description=output_text,
+                               colour=discord.Colour(0x1b2838))
             await app.send_message(message.channel, embed=em)
         elif msg[1] == 'bestseller':
             if len(msg) == 2:
@@ -97,7 +100,7 @@ async def on_message(message):
                     if previous_title == product.find('div', class_='tab_item_name').getText():
                         continue
                     previous_title = product.find('div', class_='tab_item_name').getText()
-                    if(product.find('div', class_='discount_final_price')):
+                    if (product.find('div', class_='discount_final_price')):
                         price = product.find('div', class_='discount_final_price').getText().strip()
                     else:
                         price = '기록 없음'
@@ -124,7 +127,8 @@ async def on_message(message):
                         price = ' | '
                         price += product.find('div', class_='discount_final_price').getText().strip()
                         if product.find('div', class_='discount_original_price'):
-                            price += ' ~~' + product.find('div', class_='discount_original_price').getText().strip() + '~~'
+                            price += ' ~~' + product.find('div',
+                                                          class_='discount_original_price').getText().strip() + '~~'
                     output_text += '\n' + product.find('div', class_='tab_item_name').getText() + price
 
                 em = discord.Embed(title='스팀 최고 인기 출시 예정 제품', description=output_text, colour=discord.Colour(0x1b2838))
@@ -137,7 +141,7 @@ async def on_message(message):
             output_text = '스팀의 최신 출시 제품 목록이에요.'
             for product in new_prd:
                 if product.find('div', class_='search_price').getText().strip() != '':
-                    price = product.find('div', class_='search_price').getText().strip();
+                    price = product.find('div', class_='search_price').getText().strip()
                     temp = price.split('₩')
                     if len(temp) >= 3:
                         price = '₩ {} ~~₩ {}~~'.format(temp[2], temp[1])
@@ -155,7 +159,7 @@ async def on_message(message):
             output_text = '스팀의 인기 할인 제품 목록이에요.'
             for product in new_prd:
                 if product.find('div', class_='search_price').getText().strip() != '':
-                    price = product.find('div', class_='search_price').getText().strip();
+                    price = product.find('div', class_='search_price').getText().strip()
                     temp = price.split('₩')
                     if len(temp) >= 3:
                         price = '₩ ' + temp[2] + ' ~~₩ ' + temp[1] + '~~'
@@ -182,13 +186,18 @@ async def on_message(message):
                 requested_length = len(hot_prd)
             if requested_length > 25:
                 requested_length = 25
-            em = discord.Embed(title='스팀 최다 플레이어', description='스팀의 현재 최다 플레이어 수 목록이에요.', colour=discord.Colour(0x1b2838))
+            em = discord.Embed(title='스팀 최다 플레이어', description='스팀의 현재 최다 플레이어 수 목록이에요.',
+                               colour=discord.Colour(0x1b2838))
             i = 0
             for product in hot_prd:
                 if i == requested_length:
                     break
-                name = '{} ({})'.format(product.find('a', class_='gameLink').getText(), product.find('a', class_='gameLink')['href'].replace('https://store.steampowered.com/app/', '').split('/')[0])
-                value = "현재 플레이어: {}명 | 오늘 최고 기록: {}명".format(product.find_all('span', class_='currentServers')[0].getText(), product.find_all('span', class_='currentServers')[1].getText())
+                name = '{} ({})'.format(product.find('a', class_='gameLink').getText(),
+                                        product.find('a', class_='gameLink')['href'].replace(
+                                            'https://store.steampowered.com/app/', '').split('/')[0])
+                value = "현재 플레이어: {}명 | 오늘 최고 기록: {}명".format(
+                    product.find_all('span', class_='currentServers')[0].getText(),
+                    product.find_all('span', class_='currentServers')[1].getText())
                 em.add_field(name=name, value=value, inline=False)
 
                 i += 1
@@ -207,7 +216,9 @@ async def on_message(message):
                     return
                 await app.send_message(message.channel, ":white_check_mark: 로딩 중 입니다.")
                 keys = list(id.keys())
-                news_src = requests.get('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid={}&count={}&maxlength=300&format=json'.format(keys[0], requested_length))
+                news_src = requests.get(
+                    'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid={}&count={}&maxlength=300&format=json'.format(
+                        keys[0], requested_length))
                 news_src = news_src.json()
 
                 news_text = news_src['appnews']['newsitems']
@@ -218,7 +229,8 @@ async def on_message(message):
                 for news in news_text:
                     if i > requested_length:
                         break
-                    em.add_field(name='{} - {}'.format(news['feedlabel'], news['title']), value=md(news['contents']) + ' **[자세히 보기]({})**'.format(news['url']))
+                    em.add_field(name='{} - {}'.format(news['feedlabel'], news['title']),
+                                 value=md(news['contents']) + ' **[자세히 보기]({})**'.format(news['url']))
                     i += 1
                 await app.send_message(message.channel, embed=em)
             else:
@@ -250,7 +262,8 @@ async def on_message(message):
                     await app.send_message(message.channel, ":x: 유효한 스팀 아이디를 사용해주세요.")
                     return
                 recents = requests.get(
-                    'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=B6137C92F67299965B5E6BF287ECA4AE&steamid={}&format=json'.format(steamid))
+                    'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=B6137C92F67299965B5E6BF287ECA4AE&steamid={}&format=json'.format(
+                        steamid))
                 recents = recents.json()
                 if recents['response']['total_count'] == 0:
                     await app.send_message(message.channel,
@@ -259,11 +272,12 @@ async def on_message(message):
                 em = discord.Embed(title='{} 님이 최근에 플레이하신 게임 목록이에요.'.format(msg[2]),
                                    description='지난 2주간 {}개의 게임을 플레이하셨어요.'.format(recents['response']['total_count']),
                                    colour=discord.Colour(0x1b2838))
-                total_time = 0;
+                total_time = 0
                 for text in recents['response']['games']:
                     total_time += text['playtime_2weeks']
                     em.add_field(name='{} ({})'.format(text['name'], text['appid']),
-                                 value='지난 2주간 {} 시간동안 플레이 함\n평생 동안 {} 시간동안 플레이 함'.format("%.2f" % (text['playtime_2weeks'] / 60), "%.2f" % (text['playtime_forever'] / 60)),
+                                 value='지난 2주간 {} 시간동안 플레이 함\n평생 동안 {} 시간동안 플레이 함'.format(
+                                     "%.2f" % (text['playtime_2weeks'] / 60), "%.2f" % (text['playtime_forever'] / 60)),
                                  inline=False)
                 em.add_field(name='총 플레이 시간',
                              value=msg[2] + '님은 지난 2주간 {} 시간동안 플레이하셨어요!'.format("%.2f" % (total_time / 60)))
@@ -282,7 +296,8 @@ async def on_message(message):
                     await app.send_message(message.channel, ":x: 유효한 스팀 아이디를 사용해주세요.")
                     return
                 userlib = requests.get(
-                    'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B6137C92F67299965B5E6BF287ECA4AE&steamid={}&include_appinfo=1&format=json'.format(steamid))
+                    'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B6137C92F67299965B5E6BF287ECA4AE&steamid={}&include_appinfo=1&format=json'.format(
+                        steamid))
                 userlib = userlib.json()
                 games = userlib['response']['games']
                 games = sorted(games, key=lambda game: game['playtime_forever'], reverse=True)
@@ -295,7 +310,8 @@ async def on_message(message):
                             playtime = '평생 {} 시간 플레이 함'.format('%.2f' % (game['playtime_forever'] / 60))
                             if 'playtime_2weeks' in game:
                                 playtime += '\n지난 2주 간 {} 시간 플레이 함'.format('%.2f' % (game['playtime_2weeks'] / 60))
-                            em = discord.Embed(title='{} ({})'.format(game['name'], game['appid']), description='{}\n플레이: steam://run/{}/'.format(playtime, game['appid']))
+                            em = discord.Embed(title='{} ({})'.format(game['name'], game['appid']),
+                                               description='{}\n플레이: steam://run/{}/'.format(playtime, game['appid']))
                             await app.send_message(message.channel, embed=em)
                             return
                         await app.send_message(message.channel, ":x: 게임 갯수는 정수를 사용해주세요.")
@@ -376,7 +392,7 @@ async def on_message(message):
                 statusColor = discord.Colour(0x90ba3c)
             elif xmls.find('onlineState').text == 'online':
                 statusColor = discord.Colour(0x57cbde)
-            elif xmls.find('onlineState').text == 'offline':
+            else:
                 statusColor = discord.Colour(0x898989)
             em = discord.Embed(title=xmls.find('steamID').text,
                                description=xmls.find('stateMessage').text.replace('<br/>', ': '),
@@ -401,8 +417,8 @@ async def realtime():
         await asyncio.sleep(0.1)
         if not isRealtimeAlive:
             try:
-               wst.start()
-               isRealtimeAlive = True
+                wst.start()
+                isRealtimeAlive = True
             except RuntimeError:
                 pass
         if len(realtimeQueue) < 1:
