1d31bc0ec69596c3a142bc1f839c4a3e66612313
startergate <startergate36@gmail.com>
2020-08-25 15:04:45 +0900

    misc: app.py urls와 혼동 방지

--- utils/steamapi.py
+++ utils/steamapi.py
@@ -2,11 +2,11 @@ import requests
 import xml.etree.ElementTree as et
 from bs4 import BeautifulSoup
 
-import data.urls as urls
+import data.urls as _urls
 
 
 def get_steam_id(name, want_all=False):
-    xmls = requests.get(urls.STEAM_PROFILE_XML.format(name)).text
+    xmls = requests.get(_urls.STEAM_PROFILE_XML.format(name)).text
     xmls = et.fromstring(xmls)
     try:
         xmls.find('error').text
@@ -19,7 +19,7 @@ def get_steam_id(name, want_all=False):
 
 def get_game_id(name):
     # Do Something
-    src = requests.get(urls.STEAM_SEARCH.format(name.replace(' ', '+'))).text
+    src = requests.get(_urls.STEAM_SEARCH.format(name.replace(' ', '+'))).text
     src = BeautifulSoup(src, 'html.parser')
     try:
         target = src.find_all('a', class_='search_result_row')[0]
